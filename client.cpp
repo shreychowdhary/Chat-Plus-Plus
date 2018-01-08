e087c0acdc8aedf2e6e4a13cd38387046eba1105
shreychowdhary <shrey.chowdhary1@gmail.com>
2018-01-07 21:32:14 -0500

    Send simple message

--- client.cpp
+++ client.cpp
@@ -1,9 +1,65 @@
 #include<iostream>
+#include<unistd.h>
+#include<errno.h>
+#include<cstring>
 #include<sys/types.h>
 #include<sys/socket.h>
 #include<netinet/in.h>
+#include <netdb.h>
 
-int main() {
+#define PORT "2018"
 
+#define MAXDATASIZE 100
 
+int main(int argc, char *argv[]) {
+	struct addrinfo hints, *servinfo, *p;
+	int sockfd;
+	int numbytes;
+	int getaddrinfo_result;
+	char buf[MAXDATASIZE];
+
+	if (argc != 2) {
+		std::cerr << "pass in only server ip" << std::endl;
+		return 1;
+	}
+
+	memset(&hints, 0, sizeof hints);
+	hints.ai_family = AF_UNSPEC;
+	hints.ai_socktype = SOCK_STREAM;
+
+	if((getaddrinfo_result = getaddrinfo(argv[1], PORT, &hints, &servinfo)) != 0) {
+		std::cerr << "getaddrinfo error: " << gai_strerror(getaddrinfo_result) << std::endl;
+		return 1;
+	}
+
+	for (p = servinfo; p != NULL; p = p->ai_next) {
+		if ((sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1) {
+			std::cerr << "socket create: " << strerror(errno) << std::endl;
+			continue;
+		}
+
+		if (connect(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
+			close(sockfd);
+			std::cerr << "socket connect: " << strerror(errno) << std::endl;
+			continue;
+		}
+		break;
+	}
+	
+	if (p == NULL) {
+		std::cerr << "client connect failed" << std::endl;
+		return 2;
+	}
+	
+	freeaddrinfo(servinfo);
+
+	if ((numbytes = recv(sockfd, buf, MAXDATASIZE-1, 0)) == -1) {
+		std::cerr << "recv: " << strerror(errno) << std::endl;
+		return 1;
+	}
+
+	buf[numbytes] = '\0';
+	std::cout << buf << std::endl;
+	close(sockfd);
+	return 0;
 }
--- server.cpp
+++ server.cpp
@@ -1,10 +1,78 @@
 #include<iostream>
+#include<errno.h>
+#include<unistd.h>
+#include<cstring>
 #include<sys/types.h>
 #include<sys/socket.h>
 #include<netinet/in.h>
+#include<netdb.h>
 
-int main() {
+#define PORT "2018"
 
+int main(int argc, char *argv[]) {
+	struct addrinfo hints, *servinfo, *p;
+	struct sockaddr_storage their_addr;
+	int sockfd, new_fd;
+	int getaddrinfo_result;
+	int yes = 1;
+	socklen_t sin_size;
 
+	memset(&hints, 0, sizeof hints);
+	hints.ai_family = AF_UNSPEC;
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_flags = AI_PASSIVE;
+
+	if ((getaddrinfo_result = getaddrinfo(NULL, PORT, &hints, &servinfo)) != 0) {
+		std::cout << "getaddrinfo error: " << gai_strerror(getaddrinfo_result) << std::endl;
+		return 1;
+	}
+	
+	for (p = servinfo; servinfo != NULL; servinfo = servinfo->ai_next) {
+		if ((sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1) {
+			std::cerr << "server: socket " << strerror(errno) << std::endl;
+			continue;
+		}
+
+		if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1) {
+			std::cerr << "setsockopt " << strerror(errno) << std::endl;
+			return 1;
+		}
+
+		if (bind(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
+			close(sockfd);
+			std::cerr << "setsockopt " << strerror(errno) << std::endl;
+			continue;
+		}
+		break;
+	}
+
+	freeaddrinfo(servinfo);
+
+	if (p == NULL) {
+		std::cerr << "server failed to bind " << std::endl;
+		return 1;
+	}
+
+	if (listen(sockfd, 10) == -1) {
+		std::cerr << "listen failed " << strerror(errno) << std::endl;
+		return 1;
+	}
+
+	while (true) {
+		sin_size = sizeof their_addr;
+		new_fd = accept(sockfd, (struct sockaddr *)&their_addr, &sin_size);
+		
+		if (new_fd == -1) {
+			std::cerr << "accept failed " << std::endl;
+		}	
+
+		std::cout << "connected" << std::endl;
+		close(sockfd);
+		send(new_fd, "Hey", 3, 0);
+		close(new_fd);
+		return 0;
+	}
+	return 0;
 }
+
 
